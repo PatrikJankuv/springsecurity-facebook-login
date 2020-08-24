1d64b3a34c337e4611907f757495d8819fe2babd
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-08-24 22:02:54 +0200

    Initial commit

---
+++ src/test/java/cz/ctu/fee/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.ctu.fee.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
